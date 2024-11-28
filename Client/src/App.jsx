1bb445d32b47a5bcfc6b0087ff17c1ba478a289e
Mathlps <matheussouzalopes2006@gmail.com>
2024-11-28 19:35:10 -0300

    Form Cadastro
    Formulario de Cadastro e Login feitos
    (base do projeto)

---
+++ Client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import FormCadastro from './Components/FormCadastro'
+
+function App() {
+
+  return (
+    <>
+    <div className='flex'>
+    <FormCadastro/>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
